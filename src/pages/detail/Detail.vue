974f84d5fde44ffa0a5c4a183e7508fb19c6ac1e
Freedom202 <m18827558671@163.com>
2020-06-22 14:44:49 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,18 +1,25 @@
 <template>
-    <detail-banner></detail-banner>
+    <div>  <!--Vue只允许模板里存在一个根节点。解决方案：在 <template> 中添加一个 <div>标签，之后所有的组件全部加在 <div>即可解-->
+        <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
+    </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 
 export default {
     name: 'Detail',
     components: {
-        DetailBanner
+        DetailBanner,
+        DetailHeader
     }
 }
 </script>
 
 <style scoped lang="stylus">
-
+    .content
+        height 50rem
 </style>
